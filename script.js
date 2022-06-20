05fbe4b287f540c3da46801e8d903e44784ca52a
nonnodacciaio <nonnodacciaio@gmail.com>
2022-06-20 11:13:50 +0200

    Redone

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$(document).ready(function () {
+    var mywindow = $(window);
+    var mypos = mywindow.scrollTop();
+    var up = false;
+    var newscroll;
+    mywindow.scroll(function () {
+        newscroll = mywindow.scrollTop();
+        if (newscroll > mypos && !up) {
+            $('.navbar').stop().fadeOut();
+            up = !up;
+            console.log(up);
+        } else if (newscroll < mypos && up) {
+            $('.navbar').stop().fadeIn();
+            up = !up;
+        }
+        mypos = newscroll;
+    });
+});
